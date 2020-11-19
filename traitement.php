3b4530ceae0f16152435c72428a28d13ee7d09af
Maxime Thirion <maximethirion55@gmail.com>
2020-11-19 14:50:48 +0100

    Ajax

---
+++ traitement.php
@@ -0,0 +1,28 @@
+<?php
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=alaji;charset=utf8', 'root', '');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $reponse = $bdd->query("SELECT * FROM stagiaire WHERE selected = 'false' ORDER BY RAND() LIMIT 1");
+
+    $stagiaire = $reponse->fetch();
+
+    if ($stagiaire['id']) {
+
+        $id_stagiaire = $stagiaire['id'];
+
+        echo $stagiaire['prenom'];
+
+        $bdd->query("UPDATE stagiaire SET selected = 'true' WHERE id = $id_stagiaire");
+
+        $reponse->closeCursor();
+    } else {
+
+        $bdd->query("UPDATE stagiaire SET selected = 'false'");
+    }
+?>
